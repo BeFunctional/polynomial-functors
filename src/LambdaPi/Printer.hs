b6a8ae45de5f71d6c91f217a411d2159cd352c3c
Andre Videla <andre.videla@gmail.com>
2022-10-28 19:54:44 +0100

    update pretty printer to handle encodings of pairs and coproducts

--- src/LambdaPi/Printer.hs
+++ src/LambdaPi/Printer.hs
@@ -25,16 +25,31 @@ iPrint p ii (EqElim a m mr x y eq)
                              =  iPrint p ii (Free (Global "eqElim") :$: a :$: m :$: mr :$: x :$: y :$: eq)
 iPrint p ii (Fin n)          =  iPrint p ii (Free (Global "Fin") :$: n)
 iPrint p ii (FinElim m mz ms n f)
-                               =  iPrint p ii (Free (Global "finElim") :$: m :$: mz :$: ms :$: n :$: f)
-iPrint p ii (Sigma x y)      =  iPrint p ii (Free (Global "Sigma") :$: x :$: y)
+                             =  iPrint p ii (Free (Global "finElim") :$: m :$: mz :$: ms :$: n :$: f)
+iPrint p ii (Sigma (Inf IBool) b@(Lam (Inf (If motive thn els (Inf (Bound 0))))))  =
+  if isConst 0 motive
+  then text "Either" <+> cPrint p (ii + 1) thn <+> cPrint p (ii + 1) els
+  else iPrint p ii (Free (Global "Sigma") :$: (Inf IBool) :$: b)
+iPrint p ii (Sigma x y)      =
+  if isConst 0 y
+  then lparen <> cPrint p ii x <> comma <+> cPrint p ii y <> rparen
+  else iPrint p ii (Free (Global "Sigma") :$: x :$: y)
 iPrint p ii (SigElim c y m f v)
-                               =  iPrint p ii (Free (Global "SigElim") :$: c :$: y :$: m :$: f :$: v)
-iPrint p ii IBool             =  text "Bool"
-iPrint p ii x                 =  text ("[" ++ show x ++ "]")
+                             =  if (isConst 0 f) && (isConst 1 f) -- if the return does not use its arguments
+                                then cPrint p ii f
+                                else iPrint p ii (Free (Global "SigElim") :$: c :$: y :$: m :$: f :$: v)
+iPrint p ii IBool            =  text "Bool"
+iPrint p ii (If motive thn els arg)
+                             = text "if" <+> cPrint p ii arg <+>
+                               text "then" <+> cPrint p ii thn <+>
+                               text "else" <+> cPrint p ii els
+iPrint p ii x                =  text ("[" ++ show x ++ "]")
 
 cPrint :: Int -> Int -> CTerm -> Doc
 cPrint p ii (Inf i)      = iPrint p ii i
-cPrint p ii (Lam c)      = parensIf (p > 0) (text "\\ " <> text (vars !! ii) <> text " -> " <> cPrint 0 (ii + 1) c)
+cPrint p ii (Lam c)      = if isConst 0 c
+    then parensIf (p > 0) (cPrint 0 (ii + 1) c)
+    else parensIf (p > 0) (text "\\ " <> text (vars !! ii) <> text " -> " <> cPrint 0 (ii + 1) c)
 cPrint p ii Zero         = fromNat 0 ii Zero     --  text "Zero"
 cPrint p ii (Succ n)     = fromNat 0 ii (Succ n) --  iPrint p ii (Free (Global "Succ") :$: n)
 cPrint p ii (Nil a)      = iPrint p ii (Free (Global "Nil") :$: a)
@@ -59,3 +74,81 @@ nestedForall ii ds (Inf (Pi d r)) = nestedForall (ii + 1) ((ii, d) : ds) r
 nestedForall ii ds x
   = sep [text "forall " <> sep [parensIf True (text (vars !! n) <> text " :: " <> cPrint 0 n d)
                                | (n,d) <- reverse ds] <> text " .", cPrint 0 ii x]
+
+isIConst :: Int -> ITerm -> Bool
+isIConst i (Bound n) = i /= n
+isIConst i (Ann val ty) = isConst i val && isConst i ty
+isIConst i (Pi inp out) = isConst i inp && isConst (i + 1) out
+isIConst i (fn :$: arg) = isIConst i fn && isConst i arg
+isIConst i (NatElim motive zero succ val)
+  = isConst i motive
+  && isConst i zero
+  && isConst i succ
+  && isConst i val
+isIConst i (Vec size elem) = isConst i size && isConst i elem
+isIConst i (VecElim elem motive initial nil cons val)
+  = isConst i elem
+  && isConst i motive
+  && isConst i initial
+  && isConst i nil
+  && isConst i cons
+  && isConst i val
+isIConst i (Eq ty left right) = isConst i ty && isConst i left && isConst i right
+isIConst i (EqElim a b c d e f)
+  = isConst i a
+  && isConst i b
+  && isConst i c
+  && isConst i d
+  && isConst i e
+  && isConst i f
+isIConst i (Fin n) = isConst i n
+isIConst i (FinElim a b c d e)
+  = isConst i a
+  && isConst i b
+  && isConst i c
+  && isConst i d
+  && isConst i e
+isIConst i (PolyElim a b c)
+  = isConst i a
+  && isConst i b
+  && isConst i c
+isIConst i (Sigma f s) = isConst i f && isConst i s
+isIConst i (SigElim a b c d e)
+  = isConst i a
+  && isConst i b
+  && isConst i c
+  && isConst i d
+  && (isConst i e -- either e is const
+    || e == Inf (Bound i)  -- or e is bound by the value we're looking at
+    && isConst 0 d         -- And the eliminator does not use the value
+    && isConst 1 d)        -- in which case the entire expresion is const
+
+isIConst i (If a b c d)
+  = isConst i a
+  && isConst i b
+  && isConst i c
+  && isConst i d
+isIConst i _ = True
+
+isConst :: Int -> CTerm -> Bool
+isConst i (Inf term) = isIConst i term
+isConst i (Lam body) = isConst (i + 1) body
+isConst i (Succ n) = isConst i n
+isConst i (Nil ty) = isConst i ty
+isConst i (Cons a b c d)
+  = isConst i a
+  && isConst i b
+  && isConst i c
+  && isConst i d
+isConst i (Refl left right) = isConst i left && isConst i right
+isConst i (FZero ty) = isConst i ty
+isConst i (FSucc bound n) = isConst i bound && isConst i n
+isConst i (MkPoly shapes pos) = isConst i shapes && isConst i pos
+isConst i (Comma t1 t2 p1 p2)
+  = isConst i t1
+  && isConst i t2
+  && isConst i p1
+  && isConst i p2
+isConst i _ = True
+
+isConst i _ = False
